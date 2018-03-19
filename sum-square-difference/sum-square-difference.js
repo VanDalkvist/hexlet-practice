1022817dfe23d05a1d67b45b104c06f057dffcd1
VanDalkvist <i_van@list.ru>
2018-03-19 22:41:03 +0300

    share methods;

--- sum-square-difference/sum-square-difference.js
+++ sum-square-difference/sum-square-difference.js
@@ -1,13 +1,22 @@
-module.exports = function _diffOfSums(n) {
+function _sumOfSquares(array) {
+    return array.reduce((accumulator, current) => accumulator + current ** 2, 0);
+}
+
+function _diffOfSums(n) {
     let arr = Array.from(Array(n).keys()).map(number => number + 1);
 
-    // console.log(arr);
+    console.debug(arr);
 
     let squareOfSum = arr.reduce((accumulator, current) => accumulator + current, 0) ** 2;
-    let sumOfSquares = arr.reduce((accumulator, current) => accumulator + current ** 2, 0);
+    let sumOfSquares = _sumOfSquares(arr);
 
     console.log('squareOfSum: ' + squareOfSum);
     console.log('sumOfSquares: ' + sumOfSquares);
 
     return squareOfSum - sumOfSquares;
+}
+
+module.exports = {
+    sumOfSquares: _sumOfSquares,
+    diff: _diffOfSums
 };
--- sum-square-difference/sum-square-difference.test.js
+++ sum-square-difference/sum-square-difference.test.js
@@ -1,10 +1,10 @@
 const tape = require('tape');
 
-const testable = require('./sum-square-difference');
+const { diff } = require('./sum-square-difference');
 
 tape('diff of sums', test => {
-    test.equals(testable(3), (1 + 2 + 3) ** 2 - (1 + 2 ** 2 + 3 ** 2));
-    test.equals(testable(10), 2640);
+    test.equals(diff(3), (1 + 2 + 3) ** 2 - (1 + 2 ** 2 + 3 ** 2));
+    test.equals(diff(10), 2640);
 
     test.end();
 });
